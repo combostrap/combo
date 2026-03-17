79b240eb60b7d14065fccdbd404df02fb6a80167
Nicolas GERARD <gerardnico@gmail.com>
2026-03-17 17:07:35 +0100

    feat: Added bootstrap 5.3.8

--- ComboStrap/BrandingColors.php
+++ ComboStrap/BrandingColors.php
@@ -11,43 +11,47 @@ namespace ComboStrap;
 class BrandingColors
 {
 
-    /**
-     * Do we set also the branding color on
-     * other elements ?
-     */
-    public const BRANDING_COLOR_INHERITANCE_ENABLE_CONF = "brandingColorInheritanceEnable";
-    public const BRANDING_COLOR_INHERITANCE_ENABLE_CONF_DEFAULT = 1;
-    public const PRIMARY_COLOR_CONF = "primaryColor";
+  /**
+   * Do we set also the branding color on
+   * other elements ?
+   */
+  public const BRANDING_COLOR_INHERITANCE_ENABLE_CONF = "brandingColorInheritanceEnable";
+  public const BRANDING_COLOR_INHERITANCE_ENABLE_CONF_DEFAULT = 1;
+  public const PRIMARY_COLOR_CONF = "primaryColor";
 
-    const CANONICAL = "branding-colors";
+  const CANONICAL = "branding-colors";
 
-    /**
-     * The attribute used in the model template data
-     * (used for css variables and other colors transformation)
-     */
-    const PRIMARY_COLOR_TEMPLATE_ATTRIBUTE = 'primary-color';
-    /**
-     * A color that is derived from the primary color
-     * where the contrast is good enought for reading
-     */
-    const PRIMARY_COLOR_TEXT_ATTRIBUTE = "primary-color-text";
-    /**
-     * The text color with a little bit more lightness
-     */
-    const PRIMARY_COLOR_TEXT_HOVER_ATTRIBUTE = "primary-color-text-hover";
-    public const SECONDARY_COLOR_TEMPLATE_ATTRIBUTE = 'secondary-color';
+  /**
+   * The attribute used in the model template data
+   * (used for css variables and other colors transformation)
+   */
+  const PRIMARY_COLOR_TEMPLATE_ATTRIBUTE = 'primary-color';
+  /**
+   * Rgb Value used in the template
+   */
+  const PRIMARY_COLOR_RGB_TEMPLATE_ATTRIBUTE = 'primary-color-rgb';
+  /**
+   * A color that is derived from the primary color
+   * where the contrast is good enought for reading
+   */
+  const PRIMARY_COLOR_TEXT_ATTRIBUTE = "primary-color-text";
+  /**
+   * The text color with a little bit more lightness
+   */
+  const PRIMARY_COLOR_TEXT_HOVER_ATTRIBUTE = "primary-color-text-hover";
+  public const SECONDARY_COLOR_TEMPLATE_ATTRIBUTE = 'secondary-color';
 
-    public static function getCssFormControlFocusColor(ColorRgb $primaryColor): string
-    {
+  public static function getCssFormControlFocusColor(ColorRgb $primaryColor): string
+  {
 
-        try {
-            $colorRgb = ColorSystem::toBackgroundColor($primaryColor);
-        } catch (ExceptionCompile $e) {
-            LogUtility::msg("Error on background color calculation");
-            return "";
-        }
+    try {
+      $colorRgb = ColorSystem::toBackgroundColor($primaryColor);
+    } catch (ExceptionCompile $e) {
+      LogUtility::msg("Error on background color calculation");
+      return "";
+    }
 
-        return <<<EOF
+    return <<<EOF
 .form-control:focus {
     border-color: $colorRgb;
     box-shadow: 0 0 0 0.25rem rgb({$primaryColor->getRed()} {$primaryColor->getGreen()} {$primaryColor->getBlue()} / 25%);
@@ -59,7 +63,7 @@ class BrandingColors
 EOF;
 
 
-    }
+  }
 
 
 }
--- resources/library/bootstrap/README.md
+++ resources/library/bootstrap/README.md
@@ -2,23 +2,43 @@
 
 ## About
 
-This directory contains all `Bootstrap` related resources such as Javascript and Css files used by [ComboStrap](https://combostrap.com)
+This directory contains all `Bootstrap` related resources such as JavaScript and CSS files used
+by [ComboStrap](https://combostrap.com)
 
-If you want to bring your own custom Css file, check the [custom Bootstrap article](https://combostrap.com/custom/bootrstap)
+If you want to bring your own custom Css file, check
+the [custom Bootstrap article](https://combowiki.combostrap.com/custom/bootrstap)
 
 In a nutshell, if you already have your CSS file:
 
-  * Copy your CSS file into the sub version directory (`4.4.1`, `4.5.0`, ...)
-  * Copy the [bootstrapCustomCss.json](./bootstrapStylesheet.json) to `bootstrapLocalCss.json`
-  * And adapt the file by changing or adding your values
-
+* Copy your CSS file into the sub version directory (`4.4.1`, `4.5.0`, ...)
+* Copy the [bootstrapCustomCss.json](./bootstrapStylesheet.json) to `bootstrapLocalCss.json`
+* And adapt the file by changing or adding your values
 
 ## Files
 
-  * [bootstrap.json] is a metadata file with all official bootstrap information
-  * [bootstrapCustom.json] is a metadata file with the [ComboStrap](https://combostrap.com) 16 grid theme.
-  * There is one subdirectory by `Bootstrap` release such as [4.5.0](./4.5.0)
+* `bootstrapxxx.json` is a metadata file with all official bootstrap information
+* [bootstrapCustom.json] is a metadata file with the [ComboStrap](https://combostrap.com) 16 grid theme.
+* There is one subdirectory by `Bootstrap` release such as [4.5.0](./4.5.0)
 
 ## Jquery
 
 Jquery must not be slim because the `post` http method is needed for the search box (`qsearch`)
+
+## Steps
+
+* Go to https://www.jsdelivr.com/package/npm/bootstrap
+* Chose the version to add
+* Takes the URL and SRI and writes them in:
+  * [bootstrapJavascript.json](bootstrapJavascript.json)
+  * and [bootstrapStylesheet.json](bootstrapStylesheet.json)
+* Create the version subdirectory and download the files. Example:
+
+```bash
+curl -L -o bootstrap.bundle.min.js \
+  https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js
+curl -L -o bootstrap.min.css \
+  https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css
+curl -L -o bootstrap.rtl.min.css \
+  https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.rtl.min.css
+```
+
